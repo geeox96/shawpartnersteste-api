29828e652808983220d87f63edd256ca525686ea
geeox96 <geeox96@gmail.com>
2019-12-29 16:49:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const schemaPath = ('./schema/index.graphql')
 const server = new ApolloServer({
     typeDefs: importSchema(schemaPath),
     resolvers,
+    introspection: true,
+    playground: true,
 })
+
+  
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
